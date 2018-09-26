132f942865be4d8f8b94a8de46d92be5f5c228c4
Bluecrafter35 <mauchb16@new.edvhtl.at>
2018-09-26 12:43:36 +0200

    SchuelerBL erstellt

--- SerializIntro/src/Schueler.java
+++ SerializIntro/src/Schueler.java
@@ -1,3 +1,6 @@
+
+import java.time.LocalDate;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,7 +11,49 @@
  *
  * @author Christoph Mautner
  */
-public class Schueler
+public class Schueler 
 {
+    private String name;
+    private int age;
+    private LocalDate brithday;
+
+    public Schueler(String name, int age, LocalDate brithday)
+    {
+        this.name = name;
+        this.brithday = brithday;
+        this.age = LocalDate.now().getYear()-brithday.getYear();
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public LocalDate getBrithday()
+    {
+        return brithday;
+    }
+
+    public void setBrithday(LocalDate brithday)
+    {
+        this.brithday = brithday;
+    }
+    
+    
     
 }
